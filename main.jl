5496487df26bc1b17f97449347f17e1cb9bd3000
Roki2k <filipovic.rok@gmail.com>
2024-12-28 12:02:44 +0100

    feat(fitlration): Filtration in priprava podatkov, ripserer

--- main.jl
+++ main.jl
@@ -19,20 +19,43 @@ end
 
 # ── rips ──────────────────────────────────────────────────────────────
 
-using Ripserer
-using Distances
-
-dist_matrix = pairwise(Euclidean(), data, data, dims=1);
-filtration = Ripserer.ripserer(dist_matrix);
-
-diag0 = filtration[1];
-diag1 = filtration[2];
-
+using Random
 using Plots
+using Ripserer
 
-plot(diag0)
-plot!(diag1)
-title!("Filtration")
+include("read_and_edit_data.jl")
+include("filtration.jl")
+
+data_matrix, country_labels, g = get_data()
+
+bs = []
+
+for i in 1:6
+
+    current_start = rand(1:nv(g))
+
+    filtration = create_filtration(g, current_start)
+    K = Custom(collect(filtration))
+    ph = ripserer(K)
+
+    for el in ph
+        println(el)
+    end
+
+    # ph = ripserer(K, alg = :homology, field = Rational{Int64}, dim_max = 2)
+    # open("ripserer_result_$i.jls", "w") do file
+    #     serialize(file, ph)
+    # end
+
+    country_name = country_labels[current_start]
+    b = barcode(ph)
+
+    title!("$country_name")
+    push!(bs, b)
+
+end
+
+plot(bs[1], bs[2], bs[3], bs[4], bs[5], bs[6], layout=(3, 2))  # 3 rows, 2 columns
 
 # ── geodesic ──────────────────────────────────────────────────────────
 # TODO: @rok implement this
@@ -68,4 +91,3 @@ landscape_vectors = flatten_landscapes(landscapes)
 # ╰──────────────────────────────────────────────────────────╯
 
 using MLJ
-
